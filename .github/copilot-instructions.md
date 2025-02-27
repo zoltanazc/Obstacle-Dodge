f5617a537e9ee46c9c8ca756594f52c389b090e3
zoltanazc <zoltanazc@gmail.com>
2025-02-27 20:33:41 +0100

    Fal not faling

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Obstacle-Dodge
+- Unity version: Unity 6000.0.39f1
+- Active game object:
+  - Name: Player
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/Moves.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Moves : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue , yValue, zValue);
+    }
+}
