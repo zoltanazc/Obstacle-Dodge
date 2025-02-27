930f22060f6c2f20d1c53711dfa6b0a38ae8720c
zoltanazc <zoltanazc@gmail.com>
2025-02-27 20:50:52 +0100

    Revert "Forceal mozgunk he"
    This reverts commit 16645298bf7f42ea1555f857954177f2c7e79ebc.

--- Assets/Scripts/Moves.cs
+++ Assets/Scripts/Moves.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 public class Moves : MonoBehaviour
 {
 
-    public Rigidbody playerRb;
-
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    [SerializeField] float moveSpeed = 0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,28 +12,11 @@ public class Moves : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (Input.GetKey("d"))
-        {
-            //VelocityChange, Forcemode is for changing directions easier.
-            playerRb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            playerRb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
-        }
-
-        if (Input.GetKey("w"))
-        {
-            //VelocityChange, Forcemode is for changing directions easier.
-            playerRb.AddForce(0, 0,forwardForce * Time.deltaTime, ForceMode.VelocityChange);
-        }
-
-        if (Input.GetKey("s"))
-        {
-            playerRb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
-        }
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue , yValue, zValue);
     }
 }
