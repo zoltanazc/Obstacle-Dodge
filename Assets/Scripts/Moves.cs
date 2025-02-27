16645298bf7f42ea1555f857954177f2c7e79ebc
zoltanazc <zoltanazc@gmail.com>
2025-02-27 20:48:45 +0100

    Forceal mozgunk he

--- Assets/Scripts/Moves.cs
+++ Assets/Scripts/Moves.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class Moves : MonoBehaviour
 {
 
-    [SerializeField] float moveSpeed = 0f;
+    public Rigidbody playerRb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,11 +15,28 @@ public class Moves : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue , yValue, zValue);
+        if (Input.GetKey("d"))
+        {
+            //VelocityChange, Forcemode is for changing directions easier.
+            playerRb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            playerRb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            //VelocityChange, Forcemode is for changing directions easier.
+            playerRb.AddForce(0, 0,forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            playerRb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
     }
 }
