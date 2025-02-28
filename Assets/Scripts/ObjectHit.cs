2b0ee276c9d27e86e8e2b35168507aa4cc92356b
zoltanazc <zoltanazc@gmail.com>
2025-02-28 18:59:29 +0100

    oszlopok szetbaszva

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    public Material gotHitMat;
+    public BoxCollider gotHitBoxC;
     void OnCollisionEnter()
     {
+        if(gameObject.tag != "Wall")
+        {
+            GetComponent<MeshRenderer>().material = gotHitMat;
+            gotHitBoxC.enabled = false;
+        }
+        
         
     }
 }
