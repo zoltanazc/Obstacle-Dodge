059de5933725130bdd4602386a945b58c02454a3
zoltanazc <zoltanazc@gmail.com>
2025-02-28 18:04:14 +0100

    namostelvileg jó a mozgás

--- Assets/Scripts/Moves.cs
+++ Assets/Scripts/Moves.cs
@@ -1,27 +1,43 @@
 using UnityEngine;
-
 public class Moves : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 0f;
+    public Rigidbody playerRb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() 
+    { 
 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        MovePlayer();
-    }
+         
+        if (Input.GetKey("d"))
+        {
+            //VelocityChange, Forcemode is for changing directions easier.
+            playerRb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
 
-    void MovePlayer()
-    {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue, yValue, zValue);
+        if (Input.GetKey("a"))
+        {
+            playerRb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            //VelocityChange, Forcemode is for changing directions easier.
+            playerRb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            playerRb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
     }
 }
