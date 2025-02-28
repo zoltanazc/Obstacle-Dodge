f76b44333a99d067bbce748c16a2ee3e3907894a
zoltanazc <zoltanazc@gmail.com>
2025-02-28 17:24:24 +0100

    Update Moves.cs

--- Assets/Scripts/Moves.cs
+++ Assets/Scripts/Moves.cs
@@ -8,15 +8,20 @@ public class Moves : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue , yValue, zValue);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
